4921f13ceb60c96fe42327f2946323e1ad5b6f5d
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-07 23:41:29 +0300

    floor variable conversion annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Variable floor type annotations"""
+import math
+
+
+def floor(n: float) -> int:
+    """This function converts int into floor"""
+    return int(math.floor(n))
