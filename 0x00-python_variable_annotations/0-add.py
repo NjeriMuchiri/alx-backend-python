160944eb164cf07bf3792f55d159833845521d42
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-07 23:24:59 +0300

    float variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is the python variable annotations practice"""
+
+
+def add(a: float, b: float) -> float:
+    """This function adds two float numbers"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+this is our variable annotations
