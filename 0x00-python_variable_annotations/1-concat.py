4a87ce259ac34e18d25b290b81de1cfadc6e80a6
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-07 23:31:47 +0300

    string concatenations variable annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is the python variable annotations practice"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concatenates string"""
+    return str1 + str2
