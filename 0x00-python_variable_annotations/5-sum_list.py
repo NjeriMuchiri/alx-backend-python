89a039f851e6b6ab2fde6d687f729938fb76b6a3
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-08 00:02:39 +0300

    floats sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""float sum of variable annotations"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """This function adds input lists"""
+    return sum(input_list)
